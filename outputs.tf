986d25270cb56a3b40f210b71582032986a0e3a4
Chase Pierce <syntaqx@gmail.com>
2019-05-25 08:52:26 -0600

    pluralize

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
-output "urn" {
+output "urns" {
   value = digitalocean_droplet.instance.*.urn
 }
 
-output "ipv4_address" {
+output "ipv4_addrs" {
   value = digitalocean_droplet.instance.*.ipv4_address
 }
 
-output "ipv4_address_private" {
+output "ipv4_addrs_private" {
   value = digitalocean_droplet.instance.*.ipv4_address_private
 }
 
-output "ipv6_address" {
+output "ipv6_addrs" {
   value = digitalocean_droplet.instance.*.ipv6_address
 }
 
-output "ipv6_addr_private" {
+output "ipv6_addrs_private" {
   value = digitalocean_droplet.instance.*.ipv6_address_private
 }
