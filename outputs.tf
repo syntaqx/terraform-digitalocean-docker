d2d7d145911d5bb215a98c6aaa2ae8d7b0c741e5
Chase Pierce <syntaqx@gmail.com>
2019-05-25 09:25:46 -0600

    use the entire resouce as an output

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,3 @@
-output "urns" {
-  value = digitalocean_droplet.instance[*].urn
-}
-
-output "ipv4_addrs" {
-  value = digitalocean_droplet.instance[*].ipv4_address
-}
-
-output "ipv4_addrs_private" {
-  value = digitalocean_droplet.instance[*].ipv4_address_private
-}
-
-output "ipv6_addrs" {
-  value = digitalocean_droplet.instance[*].ipv6_address
-}
-
-output "ipv6_addrs_private" {
-  value = digitalocean_droplet.instance[*].ipv6_address_private
+output "droplets" {
+  value = digitalocean_droplet.instance
 }
